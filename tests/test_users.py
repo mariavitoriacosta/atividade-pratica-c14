beedfe1dcc2245a30d17d0ff06959f32b6fe2341
JuliaVicentini <doamaralju@gmail.com>
2026-03-30 20:47:07 -0300

    test: adiciona testes de usuários

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,2 +1,42 @@
-def test_placeholder(): # teste basico so pra n quebrar a pipeline (pode tirar dps de add os testes reais)
-    assert True
+from app.services import Library
+import pytest
+
+def test_add_user_success():
+    library = Library()          
+    library.add_user(1, "Ana")   
+
+    assert 1 in library.users            
+    assert library.users[1].name == "Ana" 
+
+
+def test_add_user_name_empty():
+    library = Library()          
+
+    with pytest.raises(ValueError, match="Nome não pode ser vazio"):
+        library.add_user(1, "")  
+
+
+def test_add_user_duplicate():
+    library = Library()
+    library.add_user(1, "Ana")   
+
+    with pytest.raises(ValueError, match="Usuário já cadastrado"):
+        library.add_user(1, "Outro Nome")  
+
+
+def test_user_starts_with_no_borrowed_books():
+    library = Library()
+    library.add_user(1, "Ana")   
+
+    assert library.users[1].borrowed_books == []  
+
+
+def test_add_two_users_success():
+    library = Library()
+    library.add_user(1, "Ana")   
+    library.add_user(2, "João")  
+
+    assert 1 in library.users              
+    assert 2 in library.users             
+    assert library.users[1].name == "Ana" 
+    assert library.users[2].name == "João" 
